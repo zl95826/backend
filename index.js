5cebe685e6e4ecdbfcbcdba3dc698062cfc3647b
zl95826 <zl95826@hotmail.com>
2024-07-26 17:20:03 -0700

    fix CORS issue

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ import bodyParser from "body-parser";
 const app = express();
 app.use(bodyParser.json());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/", (req, res) => {
   res.json([
     { id: 1, name: "Monday" },
